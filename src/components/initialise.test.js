35a1249df27af516caf5afdf4086609955f2fbd9
Alan Raison <alanraison@users.sourceforge.net>
2017-07-17 22:54:53 +0100

    Fixed ignored tests

---
+++ src/components/initialise.test.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { Initialise } from './initialise';
+import Err from './error';
+import Loading from './loading';
+import Unseal from '../unseal/components';
+import Login from '../login/components';
+import Workspace from '../workspace/components';
+
+jest.mock('./error');
+jest.mock('../unseal/components');
+
+Err.mockReturnValue(() => <div/>);
+Unseal.mockReturnValue(() => <div/>);
+
+describe('The Initialise component', () => {
+  it('should show the Error compoment when there is an error', () => {
+    const e = new Error("test error")
+    const wrapper = shallow(<Initialise error={e} />);
+    expect(wrapper.find(Err)).toBePresent();
+    expect(wrapper.find(Loading)).not.toBePresent();
+    expect(wrapper.find(Unseal)).not.toBePresent();
+    expect(wrapper.find(Login)).not.toBePresent();
+    expect(wrapper.find(Workspace)).not.toBePresent();
+  });
+  it('should show the loading component when loading', () => {
+    const wrapper = shallow(<Initialise loading={true} />);
+    expect(wrapper.find(Err)).not.toBePresent();
+    expect(wrapper.find(Loading)).toBePresent();
+    expect(wrapper.find(Unseal)).not.toBePresent();
+    expect(wrapper.find(Login)).not.toBePresent();
+    expect(wrapper.find(Workspace)).not.toBePresent();
+  })
+  it('should show the unseal component when vault is sealed', () => {
+    const wrapper = shallow(<Initialise sealed={true}/>);
+    expect(wrapper.find(Err)).not.toBePresent();
+    expect(wrapper.find(Loading)).not.toBePresent();
+    expect(wrapper.find(Unseal)).toBePresent();
+    expect(wrapper.find(Login)).not.toBePresent();
+    expect(wrapper.find(Workspace)).not.toBePresent();
+  });
+  it('should show login when unuathenticated', () => {
+    const wrapper = shallow(<Initialise unauthenticated={true}/>);
+    expect(wrapper.find(Err)).not.toBePresent();
+    expect(wrapper.find(Loading)).not.toBePresent();
+    expect(wrapper.find(Unseal)).not.toBePresent();
+    expect(wrapper.find(Login)).toBePresent();
+    expect(wrapper.find(Workspace)).not.toBePresent();
+  });
+  it('should show the workspace when ready', () => {
+    const wrapper = shallow(<Initialise ready={true}/>);
+    expect(wrapper.find(Err)).not.toBePresent();
+    expect(wrapper.find(Loading)).not.toBePresent();
+    expect(wrapper.find(Unseal)).not.toBePresent();
+    expect(wrapper.find(Login)).not.toBePresent();
+    expect(wrapper.find(Workspace)).toBePresent();
+  });
+});
---
+++ src/components/page.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Page from './page';
+import Header from './header';
+import Initialise from './initialise';
+
+jest.mock('./initialise');
+
+describe('Page Component', () => {
+  Initialise.mockReturnValue(() => <div/>);
+  const wrapper = shallow(<Page store={{}}/>);
+  it('should contain a Header', () => {
+    expect(wrapper.find(Header)).toBePresent();
+  });
+  it('should contain an Initialise component', () => {
+    expect(wrapper.find(Initialise)).toBePresent();
+  });
+});
