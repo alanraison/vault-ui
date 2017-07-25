93d9cd9e4a63618cfe302c32cd119f0dcd7cfafa
Alan Raison <alanraison@users.sourceforge.net>
2017-07-25 22:22:06 +0100

    Renaming files back to .js; unseal reducer test

---
+++ src/unseal/reducers/index.test.js
@@ -0,0 +1,13 @@
+import reducer from './';
+import * as actions from '../actions';
+
+describe('the unseal reducer', () => {
+  it('should set the unseal key when an EDIT_UNSEAL_KEY action is received', () => {
+    const newState = reducer("", actions.editUnsealKey("foo"));
+    expect(newState).toEqual("foo");
+  });
+  it('should leave the state unchanged on all other actions', () => {
+    const newState = reducer("foo", { type: "ANY_OLD_ACTION", data: "bar" });
+    expect(newState).toEqual("foo");
+  });
+});
