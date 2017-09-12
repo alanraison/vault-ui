61f84119f01ed8964cb054250b407babbfffb5ad
Alan Raison <alanraison@users.sourceforge.net>
2017-09-12 23:04:52 +0100

    Working through the token API

--- src/vault-api/auth/token.js
+++ src/vault-api/auth/token.js
@@ -4,13 +4,130 @@ export default class TokenApi {
   constructor(authApi) {
     this.authApi = authApi;
     this.create = this.create.bind(this);
+    this.lookup = this.lookup.bind(this);
   }
   fetch(url, init) {
     return this.authApi.fetch(url.prefixPath('/token'), init);
   }
-  create() {
+  /**
+   * Create a new token
+   *
+   * @see {@link https://www.vaultproject.io/api/auth/token/index.html#create-token}
+   *
+   * @param options - create options
+   * @param {string} options.id - the ID to give to the new token (can only be specified by root
+   *  tokens)
+   */
+  create(options) {
     return this.fetch(new UrlSpec('/create'), {
       method: 'POST',
+      body: JSON.stringify(options),
+    });
+  }
+  /**
+   * Create a new orphan token
+   *
+   * @see {@link https://www.vaultproject.io/api/auth/token/index.html#create-token}
+   */
+  createOrphan(options) {
+    return this.fetch(new UrlSpec('/create-orphan'), {
+      method: 'POST',
+      body: JSON.stringify(options),
+    });
+  }
+  /**
+   * Lookup information about a token.
+   *
+   * @see {@link https://www.vaultproject.io/api/auth/token/index.html#lookup-a-token}
+   *
+   * @param {string} token - optional token
+   */
+  lookup(token) {
+    return this.fetch(new UrlSpec('/lookup'), {
+      method: 'POST',
+      body: JSON.stringify({ token }),
+    });
+  }
+  /**
+   * Lookup information about the current token
+   *
+   * @see {@link https://www.vaultproject.io/api/auth/token/index.html#lookup-a-token-self-}
+   */
+  lookupSelf() {
+    return this.fetch(new UrlSpec('/lookup-self'));
+  }
+  /**
+   * Returns information about the client token from the accessor.
+   *
+   * @see {@link https://www.vaultproject.io/api/auth/token/index.html#lookup-a-token-accessor}
+   */
+  lookupAccessor(accessor) {
+    return this.fetch(new UrlSpec('/lookup-accessor'), {
+      method: 'POST',
+      body: JSON.stringify({ accessor }),
+    });
+  }
+  /**
+   * Renew a token.
+   *
+   * @see {@link https://www.vaultproject.io/api/auth/token/index.html#renew-a-token}
+   * @param {string} options.token - The token to renew
+   * @param {string} options.increment - An optional requested lease increment can be provided.
+   *    This increment may be ignored.
+   */
+  renew(options) {
+    return this.fetch(new UrlSpec('/renew'), {
+      method: 'POST',
+      body: JSON.stringify(options),
+    });
+  }
+  /**
+   * Renew the current token.
+   *
+   * @see {@link https://www.vaultproject.io/api/auth/token/index.html#renew-a-token-self-}
+   * @param {string} options.increment - An optional requested lease increment can be provided.
+   *    This increment may be ignored.
+   */
+  renewSelf(options) {
+    return this.fetch(new UrlSpec('/renew-self'), {
+      method: 'POST',
+      body: JSON.stringify(options),
+    });
+  }
+  /**
+   * Revoke a token.
+   *
+   * @see {@link https://www.vaultproject.io/api/auth/token/index.html#revoke-a-token}
+   */
+  revoke(token) {
+    return this.fetch(new UrlSpec('/revoke'), {
+      method: 'POST',
+      body: JSON.stringify({ token }),
+      okCodes: 204,
+    });
+  }
+  /**
+   * Revoke the current token.
+   *
+   * @see {@link https://www.vaultproject.io/api/auth/token/index.html#revoke-a-token-self-}
+   */
+  revokeSelf() {
+    return this.fetch(new UrlSpec('/revoke-self'), {
+      method: 'POST',
+      okCodes: 204,
+    });
+  }
+  /**
+   * Revoke a token accessor.
+   *
+   * @see {@link https://www.vaultproject.io/api/auth/token/index.html#revoke-a-token-accessor}
+   * @param {string} options.accessor - accessor of the token
+   */
+  revokeAccessor(options) {
+    return this.fetch(new UrlSpec('/revoke-accessor'), {
+      method: 'POST',
+      body: JSON.stringify(options),
+      okCodes: 204,
     });
   }
 }
--- src/vault-api/auth/token.test.js
+++ src/vault-api/auth/token.test.js
@@ -1,3 +1,4 @@
+import uuid from 'uuid/v4';
 import TokenApi from './token';
 import AuthApi from './';
 import UrlSpec from '../url-spec';
@@ -7,11 +8,116 @@ AuthApi.mockImplementation(() => ({
   fetch: jest.fn(),
 }));
 
-describe('token login', () => {
-  it('should call /auth/token/create', () => {
-    const auth = new AuthApi(null);
-    const t = new TokenApi(auth);
-    t.create();
-    expect(auth.fetch).toHaveBeenCalledWith(new UrlSpec('/token/create'), { method: 'POST' });
+const auth = new AuthApi(null);
+const token = new TokenApi(auth);
+
+describe('token API', () => {
+  afterEach(() => {
+    auth.fetch.mockReset();
+  });
+  it('should be mounted at /token', () => {
+    const init = { test: uuid() };
+    token.fetch(new UrlSpec(), init);
+    expect(auth.fetch).toHaveBeenCalledWith(new UrlSpec('/token'), init);
+  });
+  describe('create', () => {
+    it('should call create token', () => {
+      token.create();
+      expect(auth.fetch).toHaveBeenCalledWith(new UrlSpec('/token/create'), { method: 'POST' });
+    });
+    it('should pass options if provided', () => {
+      const options = {
+        id: 'id',
+        role_name: 'test-role',
+      };
+      token.create(options);
+      expect(auth.fetch).toHaveBeenCalledWith(new UrlSpec('/token/create'), {
+        method: 'POST',
+        body: JSON.stringify(options),
+      });
+    });
+  });
+  describe('create-orphan', () => {
+    it('should call the create-orphan API', () => {
+      const options = {
+        display_name: 'Brian',
+      };
+      token.createOrphan(options);
+      expect(auth.fetch).toHaveBeenCalledWith(new UrlSpec('/token/create-orphan'), {
+        method: 'POST',
+        body: JSON.stringify(options),
+      });
+    });
+  });
+  describe('lookup', () => {
+    it('should call lookup for a given token', () => {
+      token.lookup('the-token');
+      expect(auth.fetch).toHaveBeenCalledWith(new UrlSpec('/token/lookup'), {
+        method: 'POST',
+        body: JSON.stringify({ token: 'the-token' }),
+      });
+    });
+  });
+  describe('lookupSelf', () => {
+    it('should call the lookup-self API', () => {
+      token.lookupSelf();
+      expect(auth.fetch).toHaveBeenCalledWith(new UrlSpec('/token/lookup-self'), undefined);
+    });
+  });
+  describe('lookupAccessor', () => {
+    it('should call the lookup-accessor API', () => {
+      token.lookupAccessor('token');
+      expect(auth.fetch).toHaveBeenCalledWith(new UrlSpec('/token/lookup-accessor'), {
+        method: 'POST',
+        body: JSON.stringify({ accessor: 'token' }),
+      });
+    });
+  });
+  describe('renew', () => {
+    it('should call the renew API', () => {
+      token.renew({ token: 'abcd', increment: '1h' });
+      expect(auth.fetch).toHaveBeenCalledWith(new UrlSpec('/token/renew'), {
+        method: 'POST',
+        body: JSON.stringify({ token: 'abcd', increment: '1h' }),
+      });
+    });
+  });
+  describe('renewSelf', () => {
+    it('should call the renew-self API', () => {
+      token.renewSelf({ increment: '1d' });
+      expect(auth.fetch).toHaveBeenCalledWith(new UrlSpec('/token/renew-self'), {
+        method: 'POST',
+        body: JSON.stringify({ increment: '1d' }),
+      });
+    });
+  });
+  describe('revoke', () => {
+    it('should call the revoke API', () => {
+      token.revoke('token');
+      expect(auth.fetch).toHaveBeenCalledWith(new UrlSpec('/token/revoke'), {
+        method: 'POST',
+        body: JSON.stringify({ token: 'token' }),
+        okCodes: 204,
+      });
+    });
+  });
+  describe('revokeSelf', () => {
+    it('should call the revoke-self API', () => {
+      token.revokeSelf();
+      expect(auth.fetch).toHaveBeenCalledWith(new UrlSpec('/token/revoke-self'), {
+        method: 'POST',
+        okCodes: 204,
+      });
+    });
+  });
+  describe('revokeAccessor', () => {
+    it('should call the revoke-accessor API', () => {
+      token.revokeAccessor({ accessor: 'accessor' });
+      expect(auth.fetch).toHaveBeenCalledWith(new UrlSpec('/token/revoke-accessor'), {
+        method: 'POST',
+        body: JSON.stringify({ accessor: 'accessor' }),
+        okCodes: 204,
+      });
+    });
   });
 });
