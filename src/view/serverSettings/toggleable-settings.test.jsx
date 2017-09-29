44473069b62034aa59528bdacede97b09724bc0b
Alan Raison <alanraison@users.sourceforge.net>
2017-09-29 18:05:21 +0100

    Tests

--- src/view/serverSettings/toggleable-settings.test.jsx
+++ src/view/serverSettings/toggleable-settings.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import Switch from 'material-ui/Switch';
+import { ListItemText } from 'material-ui/List';
 import { ToggleableSettingsComponent } from './toggleable-settings';
 
 describe('A toggleable setting', () => {
@@ -31,5 +32,25 @@ describe('A toggleable setting', () => {
     wrapper.find(Switch).find('input').simulate('change');
     expect(onEnable).not.toHaveBeenCalled();
     expect(onDisable).toHaveBeenCalled();
+  });
+  it('should set open state on click', () => {
+    const wrapper = mount(
+      <ToggleableSettingsComponent
+        enabled={false}
+        onEnable={() => null}
+        onDisable={() => null}
+      />);
+    wrapper.find(ListItemText).simulate('click');
+    expect(wrapper.state().open).toBeTruthy();
+  });
+  it('should unset open state on click when open', () => {
+    const wrapper = mount(
+      <ToggleableSettingsComponent
+        enabled={false}
+        onEnable={() => null}
+        onDisable={() => null}
+      />).setState({ open: true });
+    wrapper.find(ListItemText).simulate('click');
+    expect(wrapper.state().open).toBeFalsy();
   });
 });
