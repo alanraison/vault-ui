547f147fe20d324381d2535a48c0ae36b69e3b52
Alan Raison <alanraison@users.noreply.github.com>
2018-10-30 21:51:25 +0000

    Improved workspace

--- src/view/workspace/index.jsx
+++ src/view/workspace/index.jsx
@@ -1,34 +1,26 @@
 // @flow
 import * as React from 'react';
 import CardContent from '@material-ui/core/CardContent';
-import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import Hidden from '@material-ui/core/Hidden';
-import Card from '@material-ui/core/Card';
 import ViewCard from '../core/view-card';
 
 export function WorkspaceComponent() {
   return [
-    <ViewCard xs={12}>
+    <Hidden smDown>
+      <ViewCard md={3}>
+        <CardContent>
+          Recent Paths
+        </CardContent>
+      </ViewCard>
+    </Hidden>,
+    <ViewCard xs={12} md={8}>
       <CardContent>
-        <Grid container>
-          <Hidden smDown>
-            <Grid item md={3}>
-              <Card>
-                <CardContent>
-                  Hello
-                </CardContent>
-              </Card>
-            </Grid>
-          </Hidden>
-          <Grid item xs={9}>
-            <TextField
-              id="path"
-              label="Path"
-              fullWidth
-            />
-          </Grid>
-        </Grid>
+        <TextField
+          id="path"
+          label="Path"
+          fullWidth
+        />
       </CardContent>
     </ViewCard>,
   ];
