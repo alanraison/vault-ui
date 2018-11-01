d94dfd4c343127a89540e42e9dcb9ad0eabc2243
Alan Raison <alanraison@users.noreply.github.com>
2018-11-01 07:45:09 +0000

    fixed tests

--- src/view/workspace/index.test.jsx
+++ src/view/workspace/index.test.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { WorkspaceComponent } from '.';
-import ViewCard from '../core/view-card';
 
 describe('Workspace component', () => {
   it('should use all columns', () => {
     const wrapper = shallow(<WorkspaceComponent />);
-    expect(wrapper.find(ViewCard).prop('xs')).toBe(12);
+    expect(wrapper.find('#address').prop('xs')).toBe(12);
   });
 });
